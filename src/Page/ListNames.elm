25e490ef69513aeb495d8d26a62cdd9dd6a7d62d
Nicholas Edwards <edwardsnjd@googlemail.com>
2024-06-01 23:20:30 +0800

    feat: Beautify HTML a bit

--- src/Page/ListNames.elm
+++ src/Page/ListNames.elm
@@ -55,22 +55,54 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div []
-            [ text "Query: "
-            , input
+    div
+        [ style "padding" "1em"
+        , style "font-size" "120%"
+        ]
+        [ h2 [] [ text "Query:" ]
+        , p []
+            [ input
                 [ type_ "text"
                 , value model.query
                 , onInput UpdateQuery
-                , style "font-size" "2em"
+                , style "font-size" "1.5em"
+                , placeholder "startswith:A min:4 max:10"
                 ]
                 []
             ]
-        , div [] [ text "Filters: " ]
-        , ul [] (List.map viewFilter model.filters)
-        , div []
-            [ "Names: " ++ (List.length model.matching |> String.fromInt) ++ " of " ++ (List.length model.all |> String.fromInt) |> text ]
-        , textarea [ style "height" "50vh" ]
+        , details [ style "color" "grey" ]
+            [ summary [] [ text "Supported filters" ]
+            , dl
+                []
+                ([ ( "startswith:{prefix}", "Starts with {prefix}" )
+                 , ( "startswith:{prefix1},{prefix2},...", "Starts with either {prefix1} or {prefix2} or any other prefixes" )
+                 , ( "startswith:{from}-{to},...", "Starts between {from} and {to}, or any other supplied prefixes" )
+                 , ( "min:{length}", "Has at least {length} letters" )
+                 , ( "max:{length}", "Has at most {length} letters" )
+                 ]
+                    |> List.concatMap
+                        (\t ->
+                            [ dt [] [ code [] [ text (Tuple.first t) ] ]
+                            , dd [] [ text (Tuple.second t) ]
+                            ]
+                        )
+                )
+            ]
+        , h2 [] [ text "Filters (from query):" ]
+        , if List.length model.filters == 0 then
+            p [ style "color" "grey" ] [ text "None (type a query)" ]
+
+          else
+            ul [] (List.map viewFilter model.filters)
+        , h2 [] [ text "Matching names:" ]
+        , p [ style "color" "grey" ]
+            [ (List.length model.matching |> String.fromInt) ++ " of " ++ (List.length model.all |> String.fromInt) |> text ]
+        , textarea
+            [ style "height" "50vh"
+            , style "width" "100%"
+            , style "font-size" "0.9em"
+            , style "overflow" "scroll"
+            ]
             [ String.join "\n" model.matching |> text ]
         ]
 
